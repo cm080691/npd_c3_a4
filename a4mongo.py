463449d5ad0b068f5df242122dbd2d29d594a4b8
cm080691 <more08.chetan@gmail.com>
2016-07-30 15:23:27 +0000

    mongo assignment

--- a4mongo.py
+++ a4mongo.py
@@ -1,19 +1,21 @@
 from pymongo import MongoClient
 
 client = MongoClient()
+test_obj = {'a': 'abc', 'b': 'xyz', 'c': 'asd'}
 
 def find_object(primary_key):
     '''Finds and returns an object matching the primary key.
     Returns None if not found'
     '''
-    my_object = my_collection.find_one({'name': 'Contact Name'})
+    my_object = my_collection.find_one({'b': 'xyz'})
+    print(test_obj)
 
 def update_object(new_object):
     '''Updates an object if it exxists, inserts if does not exists'''
-    my_object = my_collection.find_one({'name': 'Contact Name'})
-    if my_object is None:
+    test_obj = my_collection.find_one({'a': 'abc'})
+    if test_obj is None:
         raise ValueError('Not Found!')
-    my_object['details'].append({'type': 'mobile', 'data': '555-555-5432'})
+    test_obj['details'].append({'type': 'mobile', 'data': '555-555-5432'})
     my_collection.update({'name': 'Contact Name'}, my_object)
 
 def remove_object(primary_key):
